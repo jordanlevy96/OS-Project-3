b611959594cbc9e10092b0c225bdbd49cb02b5b8
Chris Moranda <chrismoranda@Chriss-MacBook-Pro.local>
2018-04-27 11:44:37 -0700

    Adding stats function for thread

--- stats.c
+++ stats.c
@@ -9,19 +9,41 @@
 
 #include "blink.c"
 
-void stats(void){
-   while(1){
-      set_cursor(1,1);
-      print_hex(PC);
-      set_cursor(1,3);
-      print_int(stk_usg);
-      set_cursor(1,5);
-      print_int(stk_size);
-      set_cursor(1,7);
-      print_hex(stk_top);
-      set_cursor(1,9);
-      print_hex(stk_base);
-      set_cursor(1,11);
-      print_hex(stk_end);
-   }
+void stats(struct system_t system, uint16_t systime){
+   uint16_t stack_end;
+   uint16_t size_used;
+   struct thread_t thread;
+   thread = system->array[system->current_thread];
+   size_used = (thread->stack_base - thread->sp);
+   stack_end = (thread->stack_base - size_used);
+   set_cursor(1,1);
+   print_string("System time is: ");
+   print_int(systime);
+   set_cursor(1,2);
+   print_string("Number of threads in the system: ");
+   print_int(system->num_thread);
+   set_cursor(1,3);
+   print_string("Thread id: ");
+   print_int(thread->id);
+   set_cursor(1,4);
+   print_string("Thread name: ");
+   print_string(thread->name);
+   set_cursor(1,5);
+   print_string("Starting PC of thread: ");
+   print_hex(thread->address);
+   set_cursor(1,6);
+   print_string("Bytes used by the stack: ");
+   print_int(size_used);
+   set_cursor(1,7);
+   print_string("Stack size: ");
+   print_int(thread->stack_size); 
+   set_cursor(1,8);
+   print_int("Current top of stack: ");
+   print_hex(thread->sp);
+   set_cursor(1,9);
+   print_string("Stack base is: ");
+   print_hex(thread->stack_base);
+   set_cursor(1,10);
+   print_string("End of stack at: ");
+   print_hex(stack_end);
 }
