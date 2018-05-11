be81e5aadb11281bbcd7e018e5c2d4b1c7d2bfcc
jordanlevy96 <jordanlevy96@gmail.com>
2018-05-11 14:17:46 -0700

    moved animations

--- animations.c
+++ animations.c
@@ -2,78 +2,57 @@
 #include "globals.h"
 #endif
 
-void consumer_animation(int i){
-   int y = 1;
+void display_buffer(){
    int j;
-   for(j = 1; j < 12; j++){
+   for(j = 25; j > 14; j--){
       set_cursor(j,20);
       write_byte('|');
       set_cursor(j,24);
       write_byte('|');
    }
-   set_cursor(j-1,21);
-   print_string("___");
-   for(j = 10; j > (10-i); j--){
-      set_cursor(j,22);
-      write_byte('_');
-   }
-   delay_time(1000);
-   set_cursor(j+1, 25);
-   print_string("->");
-   delay_time(1000);
-   clear_screen();
-   for(j = 1; j < 12; j++){
-      set_cursor(j,20);
-      write_byte('|');
-      set_cursor(j,24);
-      write_byte('|');
-   }
-   set_cursor(j-1,21);
-   print_string("___");
-
-   for(j = 10; j > (11-i); j--){
-      set_cursor(j,22);
-      write_byte('_');
-   }
-   set_cursor(j+2, 27);
-   print_string("->X");
-
+   set_cursor(25, 21);
+   print_string("___"); //displays an empty buffer
+}
+void producer_animation(int i){
+    int j;
+    display_buffer();//creates empty buffer
+    for(j = 25; j > (24-i); j--){
+        set_cursor(j,22);//initializes i - 1 items in the buffer
+                         //before producing the ith item
+        write_byte('_');
+    }
+    delay_time(200);
+    set_cursor(24-i, 15);
+    print_string("->x");//show item to right of buffer
+    delay_time(200);
+    clear_screen();
+    display_buffer();
+    set_cursor(24-i, 15);
+    print_string("->");//show item entering buffer
+    for(j = 25; j > (23-i); j--){
+        set_cursor(j,22);
+        write_byte('_');//show new item in total items, totalling i items
+    }
 }
 
-void producer_animation(int i){
-   int y = 1;
-   int j;
-   for(j = 1; j < 12; j++){
-      set_cursor(j,20);
-      write_byte('|');
-      set_cursor(j,24);
-      write_byte('|');
-   }
-   set_cursor(j-1,21);
-   print_string("___");
-   for(j = 10; j > (10-i); j--){
-      set_cursor(j,22);
-      write_byte('_');
-   }
-   delay_time(1000);
-   set_cursor(j+1, 25);
-   print_string("->");
-   delay_time(1000);
-   clear_screen();
-   for(j = 1; j < 12; j++){
-      set_cursor(j,20);
-      write_byte('|');
-      set_cursor(j,24);
-      write_byte('|');
-   }
-   set_cursor(j-1,21);
-   print_string("___");
-
-   for(j = 10; j > (11-i); j--){
-      set_cursor(j,22);
-      write_byte('_');
-   }
-   set_cursor(j+2, 27);
-   print_string("->X");
+void consumer_animation(int i){
+    int j;
+    display_buffer();
+    for(j = 25; j > (22-i); j--){
+        set_cursor(j,22);//display i+1 items in buffer to start
+        write_byte('_');
+    }
+    delay_time(200);
+    set_cursor(23-i, 25);
+    print_string("->");
+    delay_time(200);
+    clear_screen();
+    display_buffer();
+    for(j = 25; j > (23-i); j--){
+        set_cursor(j,22);
+        write_byte('_');//remove an item to leave i items in buffer
+    }
+    set_cursor(24-i, 25);
+    print_string("->X");
 
 }
